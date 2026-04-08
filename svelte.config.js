792a6485d7cce19f7d66bb2f184810478cb58dab
julientora <38557341+julientora@users.noreply.github.com>
2026-04-08 21:04:29 +0000

    fix: replace adapter-auto with adapter-vercel@3 + nodejs20.x

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-    runtime: 'nodejs20.x'
-})
+			runtime: 'nodejs20.x'
+		})
 	},
 	optimizeDeps: {
 		exclude: ['layercake']
