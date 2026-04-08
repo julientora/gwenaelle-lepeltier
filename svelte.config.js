09075b6179e2583478e75cd3591ee891c300276e
julientora <38557341+julientora@users.noreply.github.com>
2026-04-08 20:59:43 +0000

    fix: force nodejs18.x runtime for Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs18.x'
+		})
 	},
 	optimizeDeps: {
 		exclude: ['layercake']
@@ -16,4 +15,3 @@ const config = {
 };
 
 export default config;
-
